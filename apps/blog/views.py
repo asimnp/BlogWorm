e8957b5f1b0126279ef72194623cfecd2c2f71c5
Asim Shrestha <asimshrestha608@gmail.com>
2021-07-07 11:04:23 +0545

    Feat[blog]: share post via email

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.core.mail import send_mail
 
 from .models import Post
+from .forms import EmailPostForm
 
 
 def post_list(request):
@@ -25,3 +27,21 @@ def post_detail(request, year, month, day, slug):
                              publish__day=day,
                              slug=slug)
     return render(request, 'blog/post/detail.html', {'post': post})
+
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status='published')
+    sent = False
+
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd['name']} recommends you to read {post.title}"
+            message = f"Read {post.title} at {post_url} \n\n{cd['name']}'s comments: {cd['comments']}"
+            send_mail(subject, message, cd['email'], [cd['to']])
+            sent = True
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'form': form, 'post': post, 'sent': sent})
--- root/settings/development.py
+++ root/settings/development.py
@@ -10,3 +10,5 @@ MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware']
 INTERNAL_IPS = [
     '127.0.0.1',
 ]
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
