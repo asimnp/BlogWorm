ae25c7871e91085be144d5e3872f417897b0dd5d
Asim Shrestha <asimshrestha608@gmail.com>
2021-07-09 09:15:35 +0545

    Feat[blog]: add full text search

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -2,11 +2,12 @@ from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.core.mail import send_mail
 from django.db.models import Count
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 
 from taggit.models import Tag
 
 from .models import Post
-from .forms import EmailPostForm, CommentForm
+from .forms import EmailPostForm, CommentForm, SearchForm
 
 
 def post_list(request, tag_slug=None):
@@ -78,3 +79,21 @@ def post_share(request, post_id):
     else:
         form = EmailPostForm()
     return render(request, 'blog/post/share.html', {'form': form, 'post': post, 'sent': sent})
+
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            search_vector = SearchVector('title', weight='A') + SearchVector('body', weight='B')
+            search_query = SearchQuery(query)
+            results = Post.published.annotate(
+                                            search=search_vector,
+                                            rank=SearchRank(search_vector, search_query)
+                                            ).filter(rank__gte=0.3).order_by('rank')
+    return render(request, 'blog/post/search.html', {'form': form, 'query': query, 'results': results})
